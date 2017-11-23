04283c792336891761e545bff04774dcb41ca8e6
Luke Bergeron <ltbergeron@gmail.com>
2017-11-22 23:11:56 -0500

    Update DFPlayerMini_Fast.h

--- DFPlayerMini_Fast.h
+++ DFPlayerMini_Fast.h
@@ -1,1 +1,30 @@
+#include "Arduino.h"
 
+#ifndef DFPlayerMini_Fast_cpp
+    #define DFPlayerMini_Fast_cpp
+
+  #define SB 0x7E //start byte
+  #define VER 0xFF //version
+  #define LEN 0x6 //number of bytes after "LEN" (except for checksum data and EB)
+  #define FEEDBACK 0 //no feedback requested
+  #define EB 0xEF //end byte
+
+  class DFPlayerMini_Fast
+  {
+    public:
+      Stream* _serial;
+  
+      uint8_t commandValue = 0;
+      uint8_t paramMSB = 0;
+      uint8_t paramLSB = 0;
+      uint8_t checksumMSB = 0;
+      uint8_t checksumLSB = 0;
+
+      void findChecksum(uint8_t commandValue, uint8_t feedback, uint8_t paramMSB, uint8_t paramLSB, uint8_t& checksumMSB, uint8_t& checksumLSB);
+      void volume(uint8_t volume, uint8_t& commandValue, uint8_t& paramMSB, uint8_t& paramLSB);
+      void loop(uint8_t volume, uint8_t& commandValue, uint8_t& paramMSB, uint8_t& paramLSB);
+      void play(uint8_t volume, uint8_t& commandValue, uint8_t& paramMSB, uint8_t& paramLSB);
+      void sendData(uint8_t commandValue, uint8_t paramMSB, uint8_t paramLSB, uint8_t checksumMSB, uint8_t checksumLSB);
+  };
+
+#endif
