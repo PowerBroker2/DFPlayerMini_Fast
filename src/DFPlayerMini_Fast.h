f8a076d2983bc0f2c967c44d758742f4da62cf91
PB2 <gitstuff2@gmail.com>
2020-04-15 02:00:22 -0400

    Add ability to stop an advert

--- src/DFPlayerMini_Fast.h
+++ src/DFPlayerMini_Fast.h
@@ -138,6 +138,7 @@ public:
 	void play(uint16_t trackNum);
 	void playFromMP3Folder(uint16_t trackNum);
 	void playAdvertisement(uint16_t trackNum);
+	void stopAdvertisement();
 	void incVolume();
 	void decVolume();
 	void volume(uint8_t volume);
