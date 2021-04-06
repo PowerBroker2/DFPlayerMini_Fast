03f77176914fc4c73240448da18092d2b1d1af4c
PB2 <gitstuff2@gmail.com>
2021-04-06 16:25:11 -0400

    Simplify example sketch and remove delays

--- examples/example/example.ino
+++ examples/example/example.ino
@@ -13,27 +13,15 @@ void setup()
 
 #if !defined(UBRR1H)
   mySerial.begin(9600);
-  myMP3.begin(mySerial);
+  myMP3.begin(mySerial, true);
 #else
   Serial1.begin(9600);
-  myMP3.begin(Serial1);
+  myMP3.begin(Serial1, true);
 #endif
   
   Serial.println("Setting volume to max");
   myMP3.volume(30);
-  delay(20);
   
-  Serial.println("Playing track 1 for 5 sec");
-  myMP3.play(1);
-  delay(5000);
-
-  Serial.println("Sleeping for 5 sec");
-  myMP3.sleep();
-  delay(5000);
-
-  Serial.println("Waking up");
-  myMP3.wakeUp();
-
   Serial.println("Looping track 1");
   myMP3.loop(1);
 }
